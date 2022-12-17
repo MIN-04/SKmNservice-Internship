e47bd136ae0988e0d9e17cc3e41e1695f620503c
MIN-04 <kimji_0424@naver.com>
2022-12-17 23:36:09 +0900

    feat: add 게시판 DB 설정
    application.yml:
    - SQL을 MYSQL로 변경
    schema.sql:
    - Board 테이블 생성
    BoardMapper.xml
    - Board 테이블 쿼리 생성
    BoardMapper.java:
    - Board 테이블 매퍼 생성

---
+++ src/main/java/SKInternship/board/Repository/BoardMapper.java
@@ -0,0 +1,47 @@
+package SKInternship.board.Repository;
+
+import SKInternship.board.domain.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+
+  /**
+   * 게시글 저장
+   * @param board - 게시글 정보
+   */
+  Long save(Board board);
+
+  /**
+   * 게시글 상세정보 조회
+   * @param id - PK
+   * @return 게시글 상세정보
+   */
+  Board findById(Long id);
+
+  /**
+   * 게시글 수정
+   * @param board - 게시글 정보
+   */
+  void update(Board board);
+
+  /**
+   * 게시글 삭제
+   * @param id - PK
+   */
+  void deleteById(Long id);
+
+  /**
+   * 게시글 리스트 조회
+   * @return 게시글 리스트
+   */
+  List<Board> findAll();
+
+  /**
+   * 게시글 수 카운팅
+   * @return 게시글 수
+   */
+  int count();
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS Members;
+DROP TABLE IF EXISTS Board;
 
-CREATE TABLE Members
-(
-    id          IDENTITY        PRIMARY KEY,
+CREATE TABLE Members(
+    id          bigint(20)    NOT NULL AUTO_INCREMENT        PRIMARY KEY,
     member_id   VARCHAR(20)     NOT NULL,
     member_pw   VARCHAR(20)     NOT NULL,
     member_name VARCHAR(20)     NOT NULL
+);
+
+CREATE TABLE Board (
+    id      bigint(20)    NOT NULL AUTO_INCREMENT        PRIMARY KEY,
+    title   varchar(100) not null,
+    content  varchar(3000) not null,
+    writer varchar(20) not null,
+    view_cnt int(11) not null,
+    created_at datetime NOT NULL DEFAULT current_timestamp()
 );
